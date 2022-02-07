94bab061492d641d3a51d3a7562ec7971def6679
NomanAyubKhan <noman450n@gmail>
2022-02-07 14:15:09 +0500

    app.use app.get comment

--- server.js
+++ server.js
@@ -37,13 +37,13 @@ app.get('/api/config/paypal', (req, res) => {
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-app.get('/', (req, res) => {
-  res.send('Server is ready');
-});
+// app.get('/', (req, res) => {
+//   res.send('Server is ready');
+// });
 
-app.use((err, _req, res, next) => {
-  res.status(500).send({ message: err.message });
-});
+// app.use((err, _req, res, next) => {
+//   res.status(500).send({ message: err.message });
+// });
 
 app.use(express.static('frontend/build'));
 
