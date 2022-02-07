acc712da91965f129d365091e23aea00d4c28a06
NomanAyubKhan <noman450n@gmail>
2022-02-07 13:39:34 +0500

    give access server to frontend

--- server.js
+++ server.js
@@ -40,8 +40,15 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 app.get('/', (req, res) => {
   res.send('Server is ready');
 });
+
 app.use((err, _req, res, next) => {
   res.status(500).send({ message: err.message });
+});
+
+app.use(express.static('frontend/build'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
 });
 
 const port = process.env.PORT;
